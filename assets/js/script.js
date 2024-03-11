b075f1331be5f257e94be7635a5a0ef492e4e424
Daniele206 <danieledeieso2004@gmail.com>
2024-03-11 15:45:03 +0100

    [add] clock

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,25 @@ createApp({
     return{
       text: 'hello word',
       img: '<img src="assets/img/01.webp" alt="">',
-    }
-  }
+      time: '00:00:00',
+    };
+  },
 
+  methods:{
+    clock(){
+      const date = new Date();
+      const h = date.getHours() < 10 ? '0'+date.getHours() : date.getHours();
+      const m = date.getMinutes() < 10 ? '0'+date.getMinutes() : date.getMinutes();
+      const s = date.getSeconds() < 10 ? '0'+date.getSeconds() : date.getSeconds();
+      this.time = `${h}:${m}:${s}`;
+    },
+    repetClock(){
+      setInterval(() => this.clock(), 1000);
+    },
+  },
+
+  mounted(){
+    this.clock();
+    this.repetClock();
+  }
 }).mount('#app')
